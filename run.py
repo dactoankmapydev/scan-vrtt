ff5d2989ff476a4daaa37157661b6cda59bd6f97
Nguyễn Đắc Toàn <30865156+dactoankmapydev@users.noreply.github.com>
2018-06-28 11:03:47 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from upload import upload
+
+
+def create_app():
+
+    app = Flask(__name__)
+    app.add_url_rule('/upload', 'upload', upload, methods=['POST', 'GET'])
+    return app
+
+serve = create_app()
+
+if __name__ == '__main__':
+    serve.run(debug=True)
