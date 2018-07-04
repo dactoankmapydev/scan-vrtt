8a92226db2c5b9dd6ea0df8a6a6e30c67096a60e
Nguyễn Đắc Toàn <30865156+dactoankmapydev@users.noreply.github.com>
2018-07-04 11:03:02 +0700

    multiprocessing

--- script.py
+++ script.py
@@ -3,22 +3,21 @@ import csv
 import requests, time, os
 from multiprocessing import Pool
 
-session = HTMLSession()
-session.browser
+
 
 def crawl(link):
 
+    session = HTMLSession()
+    session.browser
     r=session.get(link)
-    #r.html.render()
-    #rate = r.html.xpath('//*[@id="pages"]/vt-result-file/div/vt-result-header/section/header/div[1]/h1')[0].text.split('\n')[0]
-    #filename = r.html.xpath('//*[@id="file-summary"]/tbody/tr[2]/td')[0].text.split('\n')[0]
-    #filesize = r.html.xpath('//*[@id="file-summary"]/tbody/tr[3]/td')[0].text.split('\n')[0]
-    #result = r.html.xpath('//*[@id="pages"]/vt-result-file/div/vt-result-header/section/header/div[2]/h1/div')[0].text.split('\n')[1]
-    #return(filename+' '+filesize+' '+rate+' '+result)
-    #return(rate)
-    #session.get(link).html.render(sleep=0.9)
-    #return(r.html.xpath('//*[@id="pages"]/vt-result-file/div/vt-result-header/section/header/div[1]/h1'))
-    return(r.status_code)
+    r.html.render()
+
+    rate = r.html.xpath('//*[@id="pages"]/vt-result-file/div/vt-result-header/section/header/div[1]/h1')[0].text.split('\n')[0]
+    filename = r.html.xpath('//*[@id="file-summary"]/tbody/tr[2]/td')[0].text.split('\n')[0]
+    filesize = r.html.xpath('//*[@id="file-summary"]/tbody/tr[3]/td')[0].text.split('\n')[0]
+    result = r.html.xpath('//*[@id="pages"]/vt-result-file/div/vt-result-header/section/header/div[2]/h1/div')[0].text.split('\n')[1]
+    return(rate,filename,filesize,result)
+    #return(rate,result)
 
 if __name__ == '__main__':
 
@@ -31,9 +30,9 @@ if __name__ == '__main__':
         url = 'https://www.virustotal.com/#/file/'+i+'/detection'
         urls.append(url)
     print("start.....")
-    thread_pool = Pool(4)
+    thread_pool = Pool(5)
     start = time.time()
 
-
     print(thread_pool.map(crawl, urls))
+
     print(str(time.time()-start)+'seconds')
